1f7f0bbc9f5441eb3941d06a02111bfb9e758a30
VagVent <57504597+VagVent@users.noreply.github.com>
2021-02-06 17:27:10 +0200

    Add an AR cude, add the Cube Manager script
    Add AR Cube is a cube which you can put into the world in which you track a plane.
    The Cube Manager script created to check when we trach a palne then we can put there a cube. If I touch a cube which I have put at one point the the cube was deleted.

---
+++ Assets/Scripts/CubeManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class CubeManager : MonoBehaviour
+{
+    public ARRaycastManager aRRaycastManager;
+    public GameObject cubePrefab;
+
+    private List<ARRaycastHit> aRRaycastHits = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                if (Input.touchCount == 1)
+                {
+                    // AR Raycast Planes
+                    if (aRRaycastManager.Raycast(touch.position, aRRaycastHits))
+                    {
+                        var pose = aRRaycastHits[0].pose;
+                        CreateCube(pose.position);
+                        return;
+                    }
+
+                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+
+                    if (Physics.Raycast(ray, out RaycastHit hit))
+                    {
+                        if (hit.collider.tag == "cube")
+                        {
+                            DeleteCube(hit.collider.gameObject);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    /*
+     * Method to put an AR cube into the scene
+     */
+    private void CreateCube(Vector3 posotion)
+    {
+        Instantiate(cubePrefab, posotion, Quaternion.identity);
+    }
+
+    /*
+     * Method to delete the AR cube from sceen when you touch it afte your placement
+     */
+    private void DeleteCube(GameObject cubeObject)
+    {
+        Destroy(cubeObject);
+    }
+
+
+}
